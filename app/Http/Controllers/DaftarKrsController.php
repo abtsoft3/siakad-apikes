f68b2db1cd7ffaf9aa36a1264d334152821affe7
tommy <tommysuindra7@gmail.com>
2016-08-10 11:26:34 +0700

    andi

--- app/Http/Controllers/DaftarKrsController.php
+++ app/Http/Controllers/DaftarKrsController.php
@@ -136,12 +136,11 @@ class DaftarKrsController extends Controller
         $datakrs = $model->showkrs();
         $datamhs = $model->showmahasiswa();
 
-        /*$pdf = PDF::loadView('krs.show_krs', ['datamhs'=>$datamhs, 'datakrs'=>$datakrs, 'vts'=>$sem])
+        $pdf = PDF::loadView('krs.show_krs', ['datamhs'=>$datamhs, 'datakrs'=>$datakrs, 'vts'=>$sem])
                     ->setPaper('a4')
                     ->setOrientation('potrait');
 
-        return $pdf->stream();*/
-		return view('krs.show_krs', ['datamhs'=>$datamhs, 'datakrs'=>$datakrs, 'vts'=>$sem]);
+        return $pdf->stream();
     }
 
     public function store(Request $request){
