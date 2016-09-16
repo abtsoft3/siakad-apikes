34de02ab962d8a85d48b1aa095f21ba1fd5d3727
bas08 <muhammadbasri444@gmail.com>
2016-09-17 01:57:57 +0700

    komit basr

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -269,9 +269,9 @@
 			
             </div>
             <div class="row">
-              <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+             
                 @yield('content')
-              </div>
+             
             </div>
           </div>
         </div>
